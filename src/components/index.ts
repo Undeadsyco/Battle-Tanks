7ad21358f8722b56ab33f7504da5c8c806b706bb
Brandon Santana <BrandonS1123581321@gmail.com>
2024-01-11 12:04:18 -0600

    create spawner component and schema

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,17 +2,23 @@ import { Types, defineComponent } from "bitecs";
 
 export const entityComponents = {
   Tank: defineComponent<BattleTanks.Types.Components.tankSchema>({
+    id: Types.ui8,
+    spawner: Types.ui8,
     color: Types.ui8,
     hullType: Types.ui8,
     trackType: Types.ui8,
     turretType: Types.ui8,
     barrelType: Types.ui8,
   }),
-  Spawner: defineComponent(),
+  Spawner: defineComponent<BattleTanks.Types.Components.spawnerSchema>({
+    id: Types.ui8,
+    active: Types.ui8,
+    max: Types.ui8,
+  }),
 } as const;
 
 export const stateComponents = {
-  Position: defineComponent<BattleTanks.Types.Components.vectorSchema>({ x: Types.ui32, y: Types.ui32 }),
+  Position: defineComponent<BattleTanks.Types.Components.positionSchema>({ x: Types.ui32, y: Types.ui32 }),
   Angle: defineComponent<BattleTanks.Types.Components.angleSchema>({ current: Types.i16, target: Types.i16 }),
 } as const;
 
@@ -22,5 +28,5 @@ export const updateComponents = {
 } as const;
 
 export const AIComponents = {
-  CPU: defineComponent<BattleTanks.Types.Components.cpuSchema>({ timer: Types.ui16, interval: Types.ui16 })
+  CPU: defineComponent<BattleTanks.Types.Components.cpuSchema>({ timer: Types.ui16, interval: Types.ui8 })
 }
